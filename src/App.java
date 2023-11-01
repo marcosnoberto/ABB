5a5f279fa557b7486b5240e851fbb9cf7589d7cb
Marcos Vinicius Noberto de Oliveira <112330160+marcosnoberto@users.noreply.github.com>
2023-11-01 09:01:43 -0300

    V1.1 - Att. em imprimir; Criando remover

--- src/App.java
+++ src/App.java
@@ -2,14 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         Arvore arvore = new Arvore();
 
-        arvore.inserir(5);
-        arvore.inserir(3);
-        arvore.inserir(2);
-        arvore.inserir(4);
-        arvore.inserir(6);
+        arvore.inserir(250);
+        arvore.inserir(220);
+        arvore.inserir(230);
+        arvore.inserir(229);
+        arvore.inserir(260);
+        arvore.inserir(227);
+        arvore.inserir(228);
 
-        arvore.imprimirFormato2();
+        arvore.imprimir(1);
 
-        System.out.println(arvore.buscar(4).getValor());
+        // System.out.println(arvore.buscar(4).getValor());
+
+        arvore.deletar(220);
+
+        arvore.imprimir(1);
     }
 }
--- src/Arvore.java
+++ src/Arvore.java
@@ -29,19 +29,17 @@ public class Arvore {
         }
     }
 
-     public No buscar(int n) {
+    public No buscar(int n) {
         return buscaRecursiva(n, this.base);
     }
 
     private No buscaRecursiva(int n, No no) {
         if (!no.isEmpty()) {
-            if(n == no.getValor()){
+            if (n == no.getValor()) {
                 return no;
-            }
-            else if (no.getEsquerda() != null && n < no.getValor()) {
+            } else if (no.getEsquerda() != null && n < no.getValor()) {
                 return buscaRecursiva(n, no.getEsquerda());
-            } 
-            else if (no.getDireita() != null && no.getValor() < n) {
+            } else if (no.getDireita() != null && no.getValor() < n) {
                 return buscaRecursiva(n, no.getDireita());
             }
         }
@@ -49,9 +47,37 @@ public class Arvore {
         return null;
     }
 
-    public void imprimirFormato2() {
-        imprimirFormato2Recursivo(base);
-        System.out.println("");
+    public void imprimir(int forma) {
+        if (this.base != null) {
+            if (forma == 1) {
+                imprimirFormato1Recursivo(base, 0);
+            } else if (forma == 2) {
+                imprimirFormato2Recursivo(base);
+                System.out.println();
+            }
+        }
+
+    }
+
+    private void imprimirFormato1Recursivo(No no, int pos) {
+        if (!no.isEmpty()) {
+
+            for (int i = 0; i < pos; i++) {
+                System.out.print("   ");
+            }
+            System.out.print(no.getValor());
+            for (int i = 10; i > pos; i--) {
+                System.out.print("---");
+            }
+            System.out.println();
+
+            if (no.getEsquerda() != null) {
+                imprimirFormato1Recursivo(no.getEsquerda(), pos + 1);
+            }
+            if (no.getDireita() != null) {
+                imprimirFormato1Recursivo(no.getDireita(), pos + 1);
+            }
+        }
     }
 
     private void imprimirFormato2Recursivo(No no) {
@@ -59,11 +85,106 @@ public class Arvore {
             System.out.print("(" + no.getValor());
             if (no.getEsquerda() != null) {
                 imprimirFormato2Recursivo(no.getEsquerda());
-            } if (no.getDireita() != null) {
+            }
+            if (no.getDireita() != null) {
                 imprimirFormato2Recursivo(no.getDireita());
             }
             System.out.print(")");
         }
+    }
+
+    public void deletar(int n) {
+        No no = this.base;
+        while (no != null) {
+            if (no.getValor() == n) {
+                break;
+            } else if (no.getEsquerda() != null && n < no.getValor()) {
+                no = no.getEsquerda();
+            } else if (no.getDireita() != null && no.getValor() < n) {
+                no = no.getDireita();
+            } else {
+                return;
+            }
+        }
+
+        if (no.getDireita() == null && no.getEsquerda() == null) {
+            if (this.base.getValor() == n) {
+                this.base = null;
+                return;
+            } else {
+                deletarFolha(n, this.base);
+            }
+        } else if (no.getDireita() == null) {
+            System.out.println("Apenas 1 filho, da esquerda.");
+        } else if (no.getEsquerda() == null) {
+            System.out.println("Apenas 1 filho, da direita.");
+            deletar1FilhoDireita(n, this.base);
+        } else {
+            System.out.println("Dois filhos");
+        }
+    }
+
+    private void deletarFolha(int n, No no) {
+        if (n < no.getValor()) {
+            if (!no.getEsquerda().isEmpty()) {
+                if (no.getEsquerda().getValor() == n) {
+                    no.setEsquerda(null);
+                    return;
+                }
+            }
+            deletarFolha(n, no.getEsquerda());
+        } else if (no.getValor() < n) {
+            if (!no.getDireita().isEmpty()) {
+                if (no.getDireita().getValor() == n) {
+                    no.setDireita(null);
+                    return;
+                }
+            }
+            deletarFolha(n, no.getDireita());
+        }
+
+    }
+
+    private void deletar1FilhoDireita(int n, No no) {
+        if (n < no.getValor()) {
+            if (!no.getEsquerda().isEmpty()) {
+                if (no.getEsquerda().getValor() == n) {
+                    if (no.getEsquerda().getDireita().getEsquerda() == null) {
+                        no.getEsquerda().setValor(no.getEsquerda().getDireita().getValor());
+                        no.getEsquerda().setDireita(no.getEsquerda().getDireita().getDireita());
+                    } else {
+                        no.getEsquerda().setValor(excluirNoDireita(n, no.getEsquerda().getDireita()));
+                    }
+                    return;
+                }
+            }
+            deletar1FilhoDireita(n, no.getEsquerda());
+        } else if (no.getValor() < n) {
+            if (!no.getDireita().isEmpty()) {
+                if (no.getDireita().getValor() == n) {
+                    if (no.getDireita().getDireita().getEsquerda() == null) {
+                        no.getDireita().setValor(no.getDireita().getDireita().getValor());
+                        no.getDireita().setDireita(no.getEsquerda().getDireita().getDireita());
+                    } else {
+                        no.getDireita().setValor(excluirNoDireita(n, no.getDireita().getDireita()));
+                    }
+                    return;
+                }
+            }
+            deletar1FilhoDireita(n, no.getDireita());
+        }
+    }
+
+    private int excluirNoDireita(int n, No no) {
+        int valor;
+
+        if (no.getEsquerda().getEsquerda() == null) {
+            valor = no.getEsquerda().getValor();
+            no.setEsquerda(no.getEsquerda().getDireita());
+            return valor;
+        }
+
+        return excluirNoDireita(n, no.getEsquerda());
     }
 
 }
